736c604303ef81a244262c6acdd0277dfa321e63
charan2000 <44344358+charan2000@users.noreply.github.com>
2021-05-31 20:23:37 +0530

    Little Update

---
+++ index.js
@@ -0,0 +1,12 @@
+var strng1 = 'Hello world';
+console.log(strng1);
+
+var strng1 = 'Hello Javascript Tutorials';
+console.log(strng1);
+
+function stng_fncn(verb,noun,adverb){
+    var result="";
+    result+="The"+" "+verb+" "+noun+" "+adverb;
+    return result;
+}
+console.log(stng_fncn("Hello", "Mitch","That"));
